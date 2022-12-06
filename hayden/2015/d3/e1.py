9e0c66a237bc6bdfc90dae29a85286064c24a5c7
hayden <hms@hayden.computer>
2022-12-06 16:59:22 -0500

    done at work :)

---
+++ hayden/2015/d3/e1.py
@@ -0,0 +1,14 @@
+"""Perfectly Spherical Houses in a Vacuum"""
+
+
+def main():
+    """Exercise 1"""
+    with open(file="inputs.txt", mode="r", encoding="utf-8") as f_vaccuum:
+        path = f_vaccuum.readline()
+        multis = 0
+        curX = 0
+        curY = 0
+        for ch in path
+
+if __name__ == "__main__":
+    main()
