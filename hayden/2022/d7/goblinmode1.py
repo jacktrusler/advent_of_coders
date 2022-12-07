d8e9c751df5e952579674d2b6f1ee5c0ffc9be26
hayden <hms@hayden.computer>
2022-12-07 18:36:14 -0500

    Incredible hihg speed

--- hayden/2022/d7/goblinmode1.py
+++ hayden/2022/d7/goblinmode1.py
@@ -1,66 +1,39 @@
 """The Megaboy Destroyer"""
 
-from collections import Counter
-
-DISK_SPACE = 70000000
-REQD_FREE = 30000000
-
+from array import array
 
 def main():
     """Exercise 1"""
-    ductr = Counter({"/": 0})
-    with open(file="inputs.txt", mode="r", encoding="utf-8") as f_commands:
-        f_drained = False
-        output_buffer = []
-        is_buffering = False
-        working_dir = ["/"]
-        while not f_drained:
-            line = f_commands.readline()
-            if not line:
-                f_drained = True
-                break
-            if line[0] == "$":
-                if is_buffering:
-                    ls_handler(working_dir, output_buffer, ductr)
-                    output_buffer = []
-                    is_buffering = False
-                command_args = line.split()
-                if command_args[1] == "ls":
-                    is_buffering = True
-                if command_args[1] == "cd":
-                    directory = command_args[2].strip()
-                    if directory == "/":
-                        working_dir = ["/"]
-                    elif directory == "..":
-                        working_dir.pop()
-                    else:
-                        working_dir.append(directory)
+    next_inode = 1
+    ductr = array('l')
+    ductr.insert(0, 0)
+    with open(file="7_megaboy.txt", mode="r", encoding="utf-8") as f_commands:
+        #Read all lines in at once
+        commands = f_commands.readlines()
+        inode_chain = [0]
+        for line in commands:
+            #Is this a "Shell Line"
+            if line[0:4] == "$ cd":
+                directory = line[5:].strip()
+                if directory == "/":
+                    inode_chain = [0]
+                elif directory == "..":
+                    inode_chain.pop()
+                else:
+                    inode_chain.append(next_inode)
+                    ductr.insert(next_inode, 0)
+                    next_inode+=1
             else:
-                if is_buffering:
-                    output_buffer.append(line)
-    f_commands.close()
+                if line[0] == "d" or line[0] == "$":
+                    continue
+                fsize = int(line.split()[0])
+                for inode in inode_chain:
+                    ductr[inode] += fsize
     underhundk = 0
-    for _, du in ductr.items():
-        if du <= 100000:
-            underhundk += du
+    for inode_size in ductr:
+        if inode_size <= 100000:
+            underhundk += inode_size
     print(underhundk)
-
-
-def ls_handler(working_dir, output_buffer, ductr):
-    """No Depth"""
-    for outln in output_buffer:
-        ls_out = outln.strip().split()
-        if ls_out[0] == "dir":
-            ins_dir = "/".join(working_dir) + ls_out[1]
-            ductr.update({ins_dir: 0})
-        else:
-            ins_size = int(ls_out[0])
-            breadcrumbs = working_dir.copy()
-            for _ in range(len(breadcrumbs)):
-                upd_dir = "/".join(breadcrumbs)
-                ductr.update({upd_dir: ins_size})
-                breadcrumbs.pop()
-                
 
 if __name__ == "__main__":
     main()
