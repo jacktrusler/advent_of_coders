5a31da13418496a7b98fc0ba3505a30d05b96f73
Jack trusler <jtrus93@gmail.com>
2022-12-08 11:13:08 -0500

    (>&.&)> day8

--- jack/2022/2022.ts
+++ jack/2022/2022.ts
@@ -5,6 +5,7 @@ import { day4 } from "./day4"
 import { day5 } from "./day5"
 import { day6 } from "./day6"
 import { day7 } from "./day7"
+import { day8 } from "./day8"
 
 day1("./day1.txt")
 day2("./day2.txt")
@@ -12,7 +13,8 @@ day3("./day3.txt")
 day4("./day4.txt")
 day5("./day5.txt")
 day6("./day6.txt")
+day7("./day7.txt")
 const t0 = performance.now();
-day7("day7.txt")
+day8("./day8.txt")
 const t1 = performance.now();
-console.log(`Day 7 took ${t1 - t0} milliseconds.`)
+console.log(`Day 8 took ${t1 - t0} milliseconds.`)
