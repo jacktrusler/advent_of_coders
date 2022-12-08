ee81ff5ebc0603bce730f540e707de9099d04557
philophoseal <benjamin.riggs.38@gmail.com>
2022-12-08 12:19:26 -0500

    day 8 upgrades

--- ben/2022/day08/treetop_tree_house.py
+++ ben/2022/day08/treetop_tree_house.py
@@ -1,54 +1,35 @@
 import aoc
-from functools import lru_cache
 from math import prod
 import numpy as np
 from numpy.typing import NDArray
 
 
-@lru_cache
-def shift_slice(dir: tuple) -> tuple[slice, slice]:
-    default_slice = (1,-1)
-    vert = (default_slice[0] - dir[0], default_slice[1] - dir[0])
-    horz = (default_slice[0] - dir[1], default_slice[1] - dir[1])
-    vert_slice = slice(vert[0], None if vert[1] == 0 else vert[1])
-    horz_slice = slice(horz[0], None if horz[1] == 0 else horz[1])
-    return vert_slice, horz_slice
+def visible_from_edge(trees: NDArray, rot: int) -> NDArray:
+    ar = np.rot90(trees, rot)
+    rolling_max = np.maximum.accumulate(ar, axis=1)
+    padded = np.pad(rolling_max, ((0,0), (1,0)), constant_values=-1)[:,:-1]
+    return np.rot90(ar > padded, 4-rot)
 
-def shift(ar: NDArray, dir: tuple, pad: int) -> NDArray:
-    vert_slice, horz_slice = shift_slice(dir)
-    return np.pad(ar, 1, constant_values=pad)[vert_slice, horz_slice]
-
-def visible_from_edge(ar: NDArray, dir: tuple) -> NDArray:
-    def _visible(_layout):
-        shifted = shift(_layout, dir, pad=-1)
-        visible = np.where(shifted > _layout, shifted, _layout)
-        if not (_layout == visible).all():
-            return _visible(visible)
-        return visible > shifted
-    return _visible(ar)
-
-def visible_from_tree(ar: NDArray, dir: tuple) -> NDArray:
-    checker = ar.copy()
-
-    def _count_visible(_layout, count=np.full_like(ar, 0)):
-        shifted = shift(_layout, dir, pad=10)
-        count[(checker > -1) & (shifted < 10)] += 1
-        checker[checker <= shifted] = -1
-        if (checker == -1).all():
-            return count
-        return _count_visible(shifted, count)
-    return _count_visible(ar)
+def visible_from_tree(trees: NDArray, rot: int) -> NDArray:
+    ar = np.rot90(trees, rot)
+    def _on_row(i: int):
+        chop = ar[i:]
+        rolling_max = np.maximum.accumulate(chop[1:], axis=0)
+        count = np.sum(chop[0] > rolling_max, axis=0)
+        return np.where(count < len(chop)-1, count+1, count)
+    
+    vision = np.array([_on_row(i) for i in range(len(ar))])
+    return np.rot90(vision, 4-rot)
         
 
 @aoc.register(__file__)
 def answers():
     trees = np.array(aoc.read_grid(), dtype=int)
-    directions = ((1,0), (-1,0), (0,1), (0,-1))
 
-    visible = [visible_from_edge(trees, dir) for dir in directions]
+    visible = [visible_from_edge(trees, i) for i in range(4)]
     yield np.count_nonzero(np.logical_or.reduce(visible))
 
-    visible = [visible_from_tree(trees, dir) for dir in directions]
+    visible = [visible_from_tree(trees, i) for i in range(4)]
     yield np.max(prod(visible))
 
 if __name__ == '__main__':
