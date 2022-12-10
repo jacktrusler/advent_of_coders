fc474807fc667ab7dcc65e73f306164ecd9e27c3
Jack Trusler <jtrus93@gmail.com>
2022-12-10 01:07:54 -0500

    snek game gives wrong numba

---
+++ jack/2022/day9.ts
@@ -0,0 +1,91 @@
+import { readFileSync } from "fs";
+
+const answer = { part1: 0, part2: 0, }
+
+/** 
+ * Board is built by following H and dynamically changing min and max
+ * for rows and columns as H moves out of bounds
+ * @param {String[]} directions - format: <dir> <distance>
+ */
+function buildBoard(directions: string[]) {
+  let hIy = 0, hIx = 0;
+  let hIyMin = 0, hIyMax = 0, hIxMin = 0, hIxMax = 0
+
+  for (const move of directions) {
+    const [dir, xy] = move.split(' ')
+    const distance = parseInt(xy)
+    switch (dir) {
+      case 'L':
+        hIx = hIx - distance;
+        hIxMin = hIx < hIxMin ? hIx : hIxMin
+        break;
+      case 'R':
+        hIx = hIx + distance;
+        hIxMax = hIx > hIxMax ? hIx : hIxMax
+        break;
+      case 'U':
+        hIy = hIy - distance;
+        hIyMin = hIy < hIyMin ? hIy : hIyMin
+        break;
+      case 'D':
+        hIy = hIy + distance;
+        hIyMax = hIy > hIyMax ? hIy : hIyMax
+        break;
+    }
+  }
+  const boardW = hIxMax - hIxMin
+  const boardH = hIyMax - hIyMin
+
+  const board = []
+  for (let i = 0; i < boardH + 1; i++) {
+    board[i] = []
+    for (let j = 0; j < boardW + 1; j++) {
+      board[i][j] = 0;
+    }
+  }
+  //X and Y starting positions
+  const sX = Math.abs(hIxMin)
+  const sY = Math.abs(hIyMin)
+  return { board, sX, sY };
+}
+
+function day9(filePath: string) {
+  const allMoves = readFileSync(filePath).toString().replace(/\n$/, "").split('\n')
+  const { board, sX, sY } = buildBoard(allMoves)
+  // Head and Tail(s) indicies
+  let hIy = sY, hIx = sX
+  const snakeMaker = () => [
+    [hIy, hIx], [sY, sX], [sY, sX], [sY, sX], [sY, sX],
+    [sY, sX], [sY, sX], [sY, sX], [sY, sX], [sY, sX],
+  ]
+
+  let snake = snakeMaker()
+  let tmpSnake = snakeMaker()
+  for (const move of allMoves) {
+    const [dir, xy] = move.split(' ')
+    const distance = parseInt(xy)
+    for (let i = 0; i < distance; i++) {
+      if (dir === 'L') { snake[0][1]-- }
+      if (dir === 'R') { snake[0][1]++ }
+      if (dir === 'U') { snake[0][0]-- }
+      if (dir === 'D') { snake[0][0]++ }
+      for (let i = 0; i < snake.length - 1; i++) {
+        if (Math.abs(snake[i][0] - snake[i + 1][0]) === 2 ||
+          Math.abs(snake[i][1] - snake[i + 1][1]) === 2) {
+          let [newX, newY] = tmpSnake[i]
+          snake[i + 1][0] = newX
+          snake[i + 1][1] = newY
+        }
+      }
+      tmpSnake = snake.map((arr) => arr.slice())
+      //board[snake[1][0]][snake[1][1]] = board[snake[1][0]][snake[1][1]] + 1;
+      board[snake[9][0]][snake[9][1]] = board[snake[9][0]][snake[9][1]] + 1;
+    }
+  }
+  console.log(board
+    .map((row) => row.filter((num) => num > 0))
+    .reduce((acc, row) => acc + row.length, 0)
+  )
+}
+
+day9("./day9small.txt")
