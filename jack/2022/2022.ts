8f47b61da972236c010790b5fc71acf2262c31f0
Jack Trusler <jtrus93@gmail.com>
2022-12-12 10:25:10 -0500

    (>[].[])> day11

--- jack/2022/2022.ts
+++ jack/2022/2022.ts
@@ -6,7 +6,9 @@ import { day5 } from "./day05"
 import { day6 } from "./day06"
 import { day7 } from "./day07"
 import { day8 } from "./day08"
+import { day11 } from "./day11"
 
+const t0 = performance.now();
 day1("./day01.txt")
 day2("./day02.txt")
 day3("./day03.txt")
@@ -14,7 +16,7 @@ day4("./day04.txt")
 day5("./day05.txt")
 day6("./day06.txt")
 day7("./day07.txt")
-const t0 = performance.now();
 day8("./day08.txt")
+day11("./day11.txt")
 const t1 = performance.now();
-console.log(`Day 8 took ${t1 - t0} milliseconds.`)
+console.log(`all days took ${t1 - t0} milliseconds.`)
---
+++ jack/2022/day11.ts
@@ -0,0 +1,92 @@
+import { readFileSync } from "fs";
+
+const answer = { part1: 0, part2: 0 }
+
+interface Monkey {
+  monkey: number,
+  items: number[],
+  stress: string | 'old' | '+' | '*'[],
+  test: number,
+  successMonkey: number,
+  failMonkey: number,
+  inspections: number,
+}
+
+function monkeyMaker(monkey, items, stress, test, s, f): Monkey {
+  return {
+    monkey: monkey,
+    items: items,
+    stress: stress,
+    test: test,
+    successMonkey: s,
+    failMonkey: f,
+    inspections: 0,
+  }
+}
+
+function monkifier(monks: string[][]) {
+  const monkeys = []
+  for (let i = 0; i < monks.length; i = i + 6) {
+    monkeys.push(monkeyMaker(
+      parseInt(monks[i][0]),
+      [...monks[i + 1].map((item) => parseInt(item))],
+      monks[i + 2],
+      parseInt(monks[i + 3][0]),
+      parseInt(monks[i + 4][1]),
+      parseInt(monks[i + 5][1])
+    ))
+  }
+  return monkeys;
+}
+
+function monkeyBusiness(rounds: number, part: 1 | 2, m): number {
+  // Put monkeys in trees
+  const monkeys: Monkey[] = monkifier(m)
+
+  const modularity = monkeys.reduce((acc, monkey) => monkey.test * acc, 1)
+  for (let i = 0; i < rounds; i++) {
+    monkeys.forEach((monkey) => {
+      while (monkey.items.length > 0) {
+        monkey.inspections++
+        let worryLevel = monkey.items.shift()
+        if (monkey.stress[1] === '*') {
+          if (monkey.stress[2] === 'old') {
+            worryLevel = worryLevel * worryLevel
+          } else {
+            worryLevel = worryLevel * parseInt(monkey.stress[2])
+          }
+        } else {
+          worryLevel = worryLevel + parseInt(monkey.stress[2])
+        }
+
+        if (part === 1) {
+          worryLevel = Math.floor((worryLevel) / 3);
+        }
+        if (part === 2) {
+          worryLevel = worryLevel % modularity
+        }
+
+        worryLevel % monkey.test === 0
+          ? monkeys[monkey.successMonkey].items.push(worryLevel)
+          : monkeys[monkey.failMonkey].items.push(worryLevel)
+      }
+    })
+  }
+  monkeys.sort((monk1, monk2) => monk2.inspections - monk1.inspections)
+  return (monkeys[0].inspections * monkeys[1].inspections)
+}
+
+function day11(filePath: string) {
+  const mon_k = readFileSync(filePath).toString().replace(/\n$/, "").split('\n\n')
+  const reggy = /[0-9+*]+|(old)|(true)|(false)/g
+  const m = mon_k
+    .map((row) => row.split('\n'))
+    .map((inst) => inst.map((str) => str.match(reggy))).flat()
+  answer.part1 = monkeyBusiness(20, 1, m)
+  answer.part2 = monkeyBusiness(10000, 2, m)
+
+  console.log(answer)
+  return answer;
+}
+
+export { day11 }
