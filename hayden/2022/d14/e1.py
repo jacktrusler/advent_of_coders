a8c4c0de936c5e4d8bc442c5df73d2aeb277c7d9
hayden <hms@hayden.computer>
2022-12-15 00:31:31 -0500

    fixing e1 back up

--- hayden/2022/d14/e1.py
+++ hayden/2022/d14/e1.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 
 CAVE_Y = 600
-CAVE_X = 600
+CAVE_X = 10000
 def main():
     with open(file="inputs.txt", mode="r", encoding="utf-8") as f_cave:
         mapped = np.zeros((CAVE_Y,CAVE_X), dtype=np.unicode_)
@@ -14,68 +14,54 @@ def main():
             points = d_vein.split("->")
             pen_start = None
             for point in points:
-                print(f"Pen is at {pen_start}")
                 [x, y] = point.strip().split(",")
                 (x, y) = (int(x), int(y))
                 if not pen_start:
-                    print(f"Initially placing pen at ({x},{y})")
                     pen_start = (x, y)
                 else:
                     if x != pen_start[0]:
                         x_start = min(pen_start[0], x)
                         x_stop = max(pen_start[0], x)
                         for x_pos in range(x_start, x_stop+1):
-                            print(f"Pen moving to ({x_pos}, {y})")
                             mapped[y,x_pos] = "#"
                     else:
                         y_start = min(pen_start[1], y)
                         y_stop = max(pen_start[1], y)
                         for y_pos in range(y_start, y_stop+1):
-                            print(f"Pen moving to {x},{y_pos}")
                             mapped[y_pos,x] = "#"
-                    print(f"Placing pen at ({x},{y})")
                     pen_start = (x,y)
-        print_that_shit(mapped)
         full_af = False
         sand_in = 0
         while not full_af:
-            sand_in += 1
-            print(f"Dropping sand {sand_in}")
-            sand_y = 0
-            sand_x = 500
-            resting = False
-            while not resting:
-                # Attempt to fall downward
-                if mapped[sand_y+1, sand_x] == ".":
-                    sand_y += 1
-                    continue
-                else:
-                    if mapped[sand_y+1, sand_x-1] == ".":
-                    #We've hit something. Can we go left?
-                        sand_x -= 1
-                        sand_y += 1
-                        continue
-                    elif mapped[sand_y+1, sand_x+1] == ".":
-                    #How about right?
-                        sand_x += 1
-                        sand_y += 1
-                        continue
-                    else:
-                    # We must stop
-                        resting = True
-            mapped[sand_y, sand_x] = "@"
-
-
-def print_that_shit(mapped):
-    array = mapped[0:10,494:504]
-    for row in array:
-        for cel in row:
-            print(cel, sep="", end="")
-        print()
-
-
-
-
+             sand_y = 0
+             sand_x = 500
+             resting = False
+             while not resting:
+                 # Bounds checking (Cringe)
+                 if sand_y+1 >= np.size(mapped,0):
+                     full_af = True
+                     break
+                 # Attempt to fall downward
+                 if mapped[sand_y+1, sand_x] == ".":
+                     sand_y += 1
+                     continue
+                 else:
+                     if mapped[sand_y+1, sand_x-1] == ".":
+                     #We've hit something. Can we go left?
+                         sand_x -= 1
+                         sand_y += 1
+                         continue
+                     elif mapped[sand_y+1, sand_x+1] == ".":
+                     #How about right?
+                         sand_x += 1
+                         sand_y += 1
+                         continue
+                     else:
+                     # We must stop
+                         resting = True
+                         sand_in += 1
+                         mapped[sand_y, sand_x] = "@"
+        print(f"Dropped {sand_in} sand(s) undertale")
 
 if __name__ == "__main__":
     t_start = perf_counter()
