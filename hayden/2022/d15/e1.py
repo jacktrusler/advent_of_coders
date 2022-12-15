86471477504aa6fa1c73b9525e9879a1f7deec55
hayden <hms@hayden.computer>
2022-12-15 16:54:31 -0500

    Another Commit that meets the test

--- hayden/2022/d15/e1.py
+++ hayden/2022/d15/e1.py
@@ -20,36 +20,44 @@ def main():
         locstrs = f_locations.readlines()
         f_locations.close()
         sensors = []
+        fixed_ents = set()
         for locstr in locstrs:
-            [s_x,s_y,b_x,b_y] = re.findall("(-?\d)+", locstr)
+            [s_x,s_y,b_x,b_y] = re.findall("(-?\d+)", locstr)
             senpoint = (int(s_x), int(s_y))
             beaconpt = (int(b_x), int(b_y))
             sensor = Sensor(sensor=senpoint, beacon=beaconpt)
             sensors.append(sensor)
+            fixed_ents.add(senpoint)
+            fixed_ents.add(beaconpt)
         row = 2000000
-        l_max = search(row, sensors, left=True)
-        r_max = search(row,  sensors)
+        l_max = search(row, sensors, fixed_ents, left=True)
+        r_max = search(row,  sensors, fixed_ents)
         print(r_max - l_max)
 
 
-def search(row, sensors, left=False):
+def search(row, sensors, fixed_ents, left=False):
     l_max = 0
     incr = 1
+    skips = 0
     if left:
         incr = -1
     in_sight = True
     while in_sight:
         seen_once = False
         for sensor in sensors:
-            print(f"Checking coverage {row, l_max}")
+            if (row, l_max) in fixed_ents:
+                print("skip it")
+                skips += 1
+                seen_once = True
+                break
             if sensor.in_coverage((row, l_max)):
-                print(f"Covered by {sensor.pos}")
                 seen_once = True
                 break
         if not seen_once:
             in_sight = False
+            break
         l_max += incr
-    return l_max
+    return l_max - skips
 
 if __name__ == "__main__":
     t_start = perf_counter()
