2172c844a9297b91192652e630b50f348a2f9ad2
ubrec <81171322+ubrec@users.noreply.github.com>
2023-12-06 17:39:21 -0500

    days 5 and 6

--- phil/2023/day04.py
+++ phil/2023/day04.py
@@ -3,16 +3,15 @@ import utils
 
 def solve(raw_input: str):
     a = 0
-    b = 0
     lines = raw_input.splitlines()
     num_cards = [1] * len(lines)
     for i, line in enumerate(lines):
-        b += num_cards[i]
         _, card_data = line.split(":")
         winning_numbers, numbers = (set(x.split()) for x in card_data.split("|"))
         num_winners = len(winning_numbers & numbers)
         a += 2 ** (num_winners - 1) if num_winners > 0 else 0
         num_cards[i+1:i+num_winners+1] = [n + num_cards[i] for n in num_cards[i+1:i+num_winners+1]]
+    b = sum(num_cards)
     return a, b
 
 if __name__ == '__main__':
