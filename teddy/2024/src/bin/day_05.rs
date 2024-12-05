f25a5a8ac2d3a2c76058156a28b099669f3e10fa
Teddy Byron <ted@ted.ooo>
2024-12-05 13:55:08 -0500

    Update day 5

--- teddy/2024/src/bin/day_05.rs
+++ teddy/2024/src/bin/day_05.rs
@@ -2,26 +2,18 @@ use std::{cmp::Ordering, collections::HashMap};
 
 fn main() {
     let input = advent_of_code_2024::read_input!();
-    let (ordering, mut pages) = input
-        .split_once("\n\n")
-        .map(|(ordering, pages)| {
-            (
-                ordering.lines().filter_map(|l| l.split_once('|')).fold(
-                    HashMap::<_, Vec<_>>::new(),
-                    |mut acc, (k, v)| {
-                        acc.entry(k)
-                            .and_modify(|e| e.push(v))
-                            .or_insert_with(|| vec![v]);
-                        acc
-                    },
-                ),
-                pages
-                    .lines()
-                    .map(|l| l.split(',').collect::<Box<[_]>>())
-                    .collect::<Box<_>>(),
-            )
-        })
-        .unwrap();
+    let (ordering, pages) = input.split_once("\n\n").unwrap();
+    let ordering = ordering.lines().filter_map(|l| l.split_once('|')).fold(
+        HashMap::<_, Vec<_>>::new(),
+        |mut acc, (k, v)| {
+            acc.entry(k).or_default().push(v);
+            acc
+        },
+    );
+    let mut pages = pages
+        .lines()
+        .map(|l| l.split(',').collect::<Box<[_]>>())
+        .collect::<Box<_>>();
 
     let (mut p1, mut p2) = (vec![], vec![]);
     for l in &mut pages {
@@ -33,7 +25,7 @@ fn main() {
                 .filter(|&v| l.contains(v))
                 .all(|v| l.iter().position(|p| p == v).unwrap() > i)
         }) {
-            p1.push(str::parse::<i32>(l[l.len() / 2]).unwrap());
+            p1.push(l[l.len() / 2].parse().unwrap());
         } else {
             l.sort_unstable_by(|&a, &b| {
                 if ordering.get(a).unwrap().contains(&b) {
@@ -44,7 +36,7 @@ fn main() {
                     Ordering::Equal
                 }
             });
-            p2.push(str::parse::<i32>(l[l.len() / 2]).unwrap());
+            p2.push(l[l.len() / 2].parse().unwrap());
         }
     }
 
