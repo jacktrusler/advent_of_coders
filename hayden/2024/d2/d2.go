d970ea23932040953456221062f4a0d5d09bd5a1
h. smart <hms@hayden.computer>
2024-12-06 18:36:46 -0500

    Go back for day 2 even tho we dont wanna

--- hayden/2024/d2/d2.go
+++ hayden/2024/d2/d2.go
@@ -11,7 +11,7 @@ func e1(lines []string) int {
 	safeCount := 0
 	for _, line := range lines {
 		readings := strings.Fields(line)
-		fmt.Println(line)
+
 		prev := -1
 		direction := 0
 		safe := true
@@ -26,26 +26,24 @@ func e1(lines []string) int {
 			} else {
 				//Check our directionality, if we don't go either up or down toss
 				if direction == 0 {
-					fmt.Printf("P: %d, V: %d, D: %d Verdict: ", prev, val, direction)
 					if val < prev {
-						fmt.Println("Negative Movement")
+
 						direction = -1
 					} else if val > prev {
-						fmt.Println("Positive Movement")
+
 						direction = 1
 					}
 				}
 				//Check that we didn't flip directionality
 				if (direction == 1 && val < prev) || (direction == -1 && val > prev) {
-					fmt.Println("Flip")
+
 					safe = false
 					break
 				}
 				// Check that we didn't exceed movement limits
 				dist := val - prev
-				fmt.Printf("P: %d, V: %d, Dist: %d\n", prev, val, dist)
 				if dist > 3 || dist < -3 || dist == 0 {
-					fmt.Println("Exceeded Movement")
+
 					safe = false
 					break
 				}
@@ -53,75 +51,83 @@ func e1(lines []string) int {
 			}
 		}
 		if safe {
-			fmt.Println("Safe")
 			safeCount += 1
 		}
 	}
 	return safeCount
 }
+func isSafe(readings []int) bool {
+	prev := -1
+	direction := 0
+	safe := true
+	for _, reading := range readings {
+		// Set first value
+		if prev == -1 {
+			prev = reading
+		} else {
+			//Check our directionality, if we don't go either up or down toss
+			if direction == 0 {
+				if reading < prev {
+
+					direction = -1
+				} else if reading > prev {
+
+					direction = 1
+				}
+			}
+			//Check that we didn't flip directionality
+			if (direction == 1 && reading < prev) || (direction == -1 && reading > prev) {
+
+				safe = false
+				break
+			}
+			// Check that we didn't exceed movement limits
+			dist := reading - prev
+			if dist > 3 || dist < -3 || dist == 0 {
+
+				safe = false
+				break
+			}
+			prev = reading
+		}
+	}
+	return safe
+}
 func e2(lines []string) int {
 	safeCount := 0
 	for _, line := range lines {
 		readings := strings.Fields(line)
-		fmt.Println(line)
-		previousReading := 0
-		dampenAllowed := true
-		dirSet := false
-		direction := 0
-		rewindReading := 0
-		safe := true
+		readInts := make([]int, len(readings))
 		for idx, reading := range readings {
 			val, err := strconv.Atoi(reading)
 			if err != nil {
 				panic("FEED ME A PARSABLE INTEGER")
 			}
-			//If the previous reading was -1, set the value and skip to next
-			if idx == 0 {
-				fmt.Printf("Initial Reading: %d\n", val)
-				previousReading = val
-			} else {
-				// We need to check direction
-				newDirection := 0
-				if val > previousReading {
-					newDirection = 1
-				} else if val < previousReading {
-					newDirection = -1
-				}
-				// Now pull distance
-				distance := val - previousReading
-				fmt.Printf("Rew: %d Prev: %d, Val: %d, Dir: %d, NewDir: %d, Dist: %d\n", rewindReading, previousReading, val, direction, newDirection, distance)
-				//The following events would be disqualifying:
-				if distance > 3 || distance < -3 || distance == 0 {
-					fmt.Printf("Disqualified \n")
-					if dampenAllowed {
-						fmt.Printf("Dampening\n")
-						dampenAllowed = false
-						previousReading = rewindReading
-						continue
-					}
-					safe = false
-				}
-				if newDirection != direction && dirSet {
-					fmt.Printf("Disqualified \n")
-					if dampenAllowed {
-						fmt.Printf("Dampening\n")
-						dampenAllowed = false
-						previousReading = rewindReading
-						continue
-					}
-					safe = false
-				}
-				if !dirSet {
-					direction = newDirection
-				}
-				fmt.Printf("updating previous reading\n")
-				rewindReading = previousReading
-				previousReading = val
-			}
+			readInts[idx] = val
 		}
-		if safe {
-			fmt.Printf("Safe \n")
-			safeCount += 1
+		if isSafe(readInts) {
+			safeCount++
+		} else {
+			fixable := false
+			for cutInd := 0; cutInd < len(readInts) && !fixable; cutInd++ {
+				withCut := make([]int, 0)
+				sliceable := make([]int, len(readInts))
+				copy(sliceable, readInts)
+				switch cutInd {
+				case 0:
+					withCut = append(withCut, sliceable[1:]...)
+				case len(readInts) - 1:
+					withCut = append(withCut, sliceable[:len(readInts)-1]...)
+				default:
+					withCut = append(sliceable[:cutInd], sliceable[cutInd+1:]...)
+				}
+				if isSafe(withCut) {
+					fixable = true
+				}
+			}
+			if fixable {
+				safeCount++
+			}
 		}
 	}
 	return safeCount
@@ -132,6 +138,8 @@ func main() {
 		panic("Failed to open file ./input.txt")
 	}
 	lines := strings.Split(string(raw), "\n")
-	safeCount := e2(lines)
-	fmt.Println(safeCount)
+	safeCount := e1(lines)
+	fmt.Printf("For Exercise 1: Safe %d\n", safeCount)
+	safeCount = e2(lines)
+	fmt.Printf("For Exercise 2: Safe %d\n", safeCount)
 }
