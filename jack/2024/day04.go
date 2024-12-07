42181b764beac04d713e51929d00b70c4cc353f6
jacktrusler <jtrus93@gmail.com>
2024-12-06 23:50:41 -0500

    sus solution for sus word search

--- jack/2024/day04.go
+++ jack/2024/day04.go
@@ -7,14 +7,14 @@ import (
 )
 
 var (
-	ws []string
+	ws      []string
+	total   int
+	totalP2 int
 )
 
 func makeSusSearch(input string) {
 	lines := strings.Split(input, "\n")
-	// pad with amogus
 	runeArr := make([]rune, len(lines[0])+2)
-	// sus
 	for i := range runeArr {
 		runeArr[i] = '0'
 	}
@@ -32,7 +32,7 @@ func makeSusSearch(input string) {
 	ws = wordSearch
 }
 
-func boggleStyle(y, x int, word string, total *int) {
+func boggleStyle(y, x int, word string) {
 	//exit conditions
 	if ws[y][x] == '0' {
 		return
@@ -46,43 +46,64 @@ func boggleStyle(y, x int, word string, total *int) {
 
 	word = word + string(ws[y][x])
 	if word == "XMAS" {
-		*total++
+		total++
 		return
 	}
 
 	// W
-	boggleStyle(y, x-1, word, total)
+	boggleStyle(y, x-1, word)
 	// NW
-	boggleStyle(y-1, x-1, word, total)
+	boggleStyle(y-1, x-1, word)
 	// N
-	boggleStyle(y-1, x, word, total)
+	boggleStyle(y-1, x, word)
 	// NE
-	boggleStyle(y-1, x+1, word, total)
+	boggleStyle(y-1, x+1, word)
 	// E
-	boggleStyle(y, x+1, word, total)
+	boggleStyle(y, x+1, word)
 	// SE
-	boggleStyle(y+1, x+1, word, total)
+	boggleStyle(y+1, x+1, word)
 	// S
-	boggleStyle(y+1, x, word, total)
+	boggleStyle(y+1, x, word)
 	// SW
-	boggleStyle(y+1, x-1, word, total)
+	boggleStyle(y+1, x-1, word)
 
 	return
 }
 
-func day4part1(input string) {
-	makeSusSearch(input)
-	var total int
-	for y := 1; y < len(ws)-2; y++ {
-		for x, rune := range ws[y] {
-			var runningTotal int
+func travel(y, x, dy, dx int, w string) {
+	if w == "XMAS" {
+		total++
+	}
+	if ws[y][x] == '0' {
+		return
+	}
+	if len(w) == 4 {
+		return
+	}
+	travel(y+dy, x+dx, dy, dx, w+string(ws[y][x]))
+}
+
+func day4part1() {
+	for y := 1; y < len(ws)-1; y++ {
+		line := ws[y]
+		for x, rune := range line {
 			if rune == 'X' {
-				boggleStyle(y, x, "", &runningTotal)
-				runes := []byte(ws[y])
-				runes[x] = '0'
-				ws[y] = string(runes)
-				total += runningTotal
-				runningTotal = 0
+				// NW
+				travel(y-1, x-1, -1, -1, "X")
+				// N
+				travel(y-1, x, -1, 0, "X")
+				// NE
+				travel(y-1, x+1, -1, 1, "X")
+				// E
+				travel(y, x+1, 0, 1, "X")
+				// SE
+				travel(y+1, x+1, 1, 1, "X")
+				// S
+				travel(y+1, x, 1, 0, "X")
+				// SW
+				travel(y+1, x-1, 1, -1, "X")
+				// W
+				travel(y, x-1, 0, -1, "X")
 			}
 		}
 	}
@@ -90,13 +111,41 @@ func day4part1(input string) {
 
 }
 
-func day4part2(input string) {
+func day4part2() {
+	for y := 1; y < len(ws)-1; y++ {
+		line := ws[y]
+		for x, rune := range line {
+			if rune == 'A' {
+
+				runeMap := make(map[string]byte)
+				runeMap["NW"] = ws[y-1][x-1]
+				runeMap["NE"] = ws[y-1][x+1]
+				runeMap["SW"] = ws[y+1][x-1]
+				runeMap["SE"] = ws[y+1][x+1]
+
+				if runeMap["NW"] == 'M' && runeMap["NE"] == 'M' && runeMap["SW"] == 'S' && runeMap["SE"] == 'S' {
+					totalP2++
+				}
+				if runeMap["NW"] == 'M' && runeMap["NE"] == 'S' && runeMap["SW"] == 'M' && runeMap["SE"] == 'S' {
+					totalP2++
+				}
+				if runeMap["NW"] == 'S' && runeMap["NE"] == 'M' && runeMap["SW"] == 'S' && runeMap["SE"] == 'M' {
+					totalP2++
+				}
+				if runeMap["NW"] == 'S' && runeMap["NE"] == 'S' && runeMap["SW"] == 'M' && runeMap["SE"] == 'M' {
+					totalP2++
+				}
+			}
+		}
+	}
+	fmt.Println(totalP2)
 }
 
 func Day4() {
-	input := goutils.FileAsString("./input/04-example.txt")
+	input := goutils.FileAsString("./input/2024-04-input.txt")
+	makeSusSearch(input)
 	fmt.Println("----- Part 1 -----")
-	day4part1(input)
+	day4part1()
 	fmt.Println("----- Part 2 -----")
-	day4part2(input)
+	day4part2()
 }
