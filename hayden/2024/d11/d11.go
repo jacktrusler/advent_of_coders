2d6611286a8bc2f922963493f64d8d1458473052
h. smart <hms@hayden.computer>
2024-12-11 12:58:51 -0500

    i push

--- hayden/2024/d11/d11.go
+++ hayden/2024/d11/d11.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
+func splitRock(rock int, digits int) (int, int) {
+	splitter := math.Pow10(digits / 2)
+	return rock / int(splitter), rock % int(splitter)
+}
 func digitCount(rock int) int {
-	for (digit)
+	digits := 0
+	for digits = 0; rock > 0; rock /= 10 {
+		digits++
+	}
+	return digits
 }
 func blink(rocks []int) []int {
 	newState := make([]int, len(rocks))
 	newStateCur := 0
-	for rockIdx, rock := range rocks {
+	for _, rock := range rocks {
 		if rock == 0 {
 			newState[newStateCur] = 1
 			continue
 		}
-		if digitCount(rock) % 2 == 0 {
-
+		digits := digitCount(rock)
+		if digitCount(rock)%2 == 0 {
+			leftRock, rightRock := splitRock(rock, digits)
+			fmt.Printf("Split %d, %d & %d\n", rock, leftRock, rightRock)
+			newState1
 		}
 	}
 	return newState
@@ -44,5 +56,6 @@ func main() {
 		panic("Bad File")
 	}
 	stones := processInput(raw)
+	blink(stones)
 	fmt.Println(stones)
 }
